a081e1426bf1f830a8d74bbcf569dda20a9652af
wuchengxing <1449697757@qq.com>
2022-09-08 10:35:34 +0800

    feat：docker常用命令

---
+++ scripts/shell/docker/docker-about.sh
@@ -0,0 +1,23 @@
+docker run [OPTIONS] IMAGE [COMMAND] [ARG...]
+
+#【docker run  设置项   镜像名  】 镜像启动运行的命令（镜像里面默认有的，一般不会写）
+
+# -d：后台运行
+# --restart=always: 开机自启
+docker run --name=mynginx   -d  --restart=always -p  88:80   nginx
+
+# 查看正在运行的容器
+docker ps
+# 查看所有
+docker ps -a
+# 删除停止的容器
+docker rm  容器id/名字
+docker rm -f mynginx   #强制删除正在运行中的
+
+#停止容器
+docker stop 容器id/名字
+#再次启动
+docker start 容器id/名字
+
+#应用开机自启
+docker update 容器id/名字 --restart=always
